c50286793a7af93275abc42bc6c82e70955da216
fabioBM-Dev <faleobamu@gmail.com>
2025-09-28 00:33:21 -0500

    coriige URL api Update script.js

--- public/inventarios/script.js
+++ public/inventarios/script.js
@@ -2,7 +2,7 @@
 // Configuración base
 // ============================
 
-const API_URL = "http://localhost:3002/api/inventarios";
+const API_URL = ""https://3.16.215.161:4033/api/inventarios";
 
 // ============================
 // Validación de formulario
@@ -210,3 +210,4 @@ document.addEventListener("DOMContentLoaded", () => {
     cargarInventarios();
 
 });
+
